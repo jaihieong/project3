d616576739f03aa1d66c8dcc78895b79dc86eb13
Pseudo Ninja <jaihieong88@gmail.com>
2019-05-20 19:57:00 -0400

    react app added

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder="name" />
+        <input type="email" placeholder="email" />
+        <input type="password" placeholder="password" />
+        <button onClick={}>Submit</button>
+      
+      </form>
+    </div>
+  );
+}
+
+export default App;

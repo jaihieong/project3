871aee032ecc7a241524fea17c0f74ba89f6bfe9
Pseudo Ninja <jaihieong88@gmail.com>
2019-05-24 18:57:46 -0400

    front-end files add with alerts when registering

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Fragment} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Alert from './components/layout/Alert';
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <form>
-        <input type="text" placeholder="name" />
-        <input type="email" placeholder="email" />
-        <input type="password" placeholder="password" />
-        <button onClick={}>Submit</button>
-      
-      </form>
-    </div>
-  );
-}
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className="container">
+          <Alert />
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
+);
+
 
 export default App;

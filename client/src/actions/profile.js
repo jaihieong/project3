df45ad65f8351e5ffe7052023d774611903cde1e
Pseudo Ninja <jaihieong88@gmail.com>
2019-05-26 13:29:01 -0400

    added experience and education

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -3,7 +3,8 @@ import { setAlert } from './alert';
 
 import {
   GET_PROFILE,
-  PROFILE_ERROR
+  PROFILE_ERROR,
+  UPDATE_PROFILE
 } from './types';
 
 // Get current users profile
@@ -47,6 +48,82 @@ export const createProfile = (formData, history, edit = false) => async dispatch
     if(!edit) {
       history.push("/dashboard");
     }
+
+  } catch (err) {
+    console.log(err.response);
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status}
+    });
+
+  }
+}
+
+// Add Experience
+export const addExperience = (formData, history) => async dispatch => {
+  try {
+    // since we sending the Data we need the config object
+    const config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+
+    // we making post request to /api/profile
+    const res = await axios.put("/api/profile/experience", formData, config);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert('Experience Added', "success"));
+    history.push("/dashboard");
+  
+
+  } catch (err) {
+    console.log(err.response);
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status}
+    });
+
+  }
+}
+
+// Add Education
+export const addEducation = (formData, history) => async dispatch => {
+  try {
+    // since we sending the Data we need the config object
+    const config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+
+    // we making post request to /api/profile
+    const res = await axios.put("/api/profile/education", formData, config);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert('Education Added', "success"));
+    history.push("/dashboard");
+  
 
   } catch (err) {
     console.log(err.response);
--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -56,46 +56,72 @@ const CreateProfile = ({ createProfile, history }) => {
             <small>* = required field</small>
             <form className="form" onSubmit={e => onSubmit(e)}>
                 <div className="form-group">
-                <select name="status" value={status} onChange={e => onChange(e)}>
-                    <option value="0">* Select Professional Status</option>
-                    <option value="Developer">Developer</option>
-                    <option value="Junior Developer">Junior Developer</option>
-                    <option value="Senior Developer">Senior Developer</option>
-                    <option value="Manager">Manager</option>
-                    <option value="Student or Learning">Student or Learning</option>
-                    <option value="Instructor">Instructor or Teacher</option>
-                    <option value="Intern">Intern</option>
-                    <option value="Other">Other</option>
-                </select>
-                <small className="form-text"
-                    >Give us an idea of where you are at in your career</small
-                >
+                    <select name="status" value={status} onChange={e => onChange(e)}>
+                        <option value="0">* Select Professional Status</option>
+                        <option value="Developer">Developer</option>
+                        <option value="Junior Developer">Junior Developer</option>
+                        <option value="Senior Developer">Senior Developer</option>
+                        <option value="Manager">Manager</option>
+                        <option value="Student or Learning">Student or Learning</option>
+                        <option value="Instructor">Instructor or Teacher</option>
+                        <option value="Intern">Intern</option>
+                        <option value="Other">Other</option>
+                    </select>
+                    <small className="form-text"
+                        >Give us an idea of where you are at in your career
+                    </small>
                 </div>
+                
                 <div className="form-group">
-                <input type="text" placeholder="Company" name="company" value={company} onChange={e => onChange(e)} />
-                <small className="form-text"
-                    >Could be your own company or one you work for</small
-                >
+                    <input 
+                        type="text" placeholder="Company" name="company" value={company} onChange={e => onChange(e)} />
+                    <small 
+                        className="form-text"
+                    >
+                        Could be your own company or one you work for
+                    </small>
+                    </div>
+
+                    <div className="form-group">
+                    <input 
+                        type="text" 
+                        placeholder="Website" 
+                        name="website" 
+                        value={website} 
+                        onChange={e => onChange(e)} 
+                    />
+                    <small className="form-text">
+                        Could be your own or a company website
+                    </small>
                 </div>
+
                 <div className="form-group">
-                <input type="text" placeholder="Website" name="website" value={website} onChange={e => onChange(e)} />
-                <small className="form-text"
-                    >Could be your own or a company website</small
-                >
+                    <input 
+                        type="text" 
+                        placeholder="Location" 
+                        name="location" 
+                        value={location} 
+                        onChange={e => onChange(e)} 
+                    />
+                    <small className="form-text">
+                        City & state suggested (eg. Boston, MA)
+                    </small>
                 </div>
+
                 <div className="form-group">
-                <input type="text" placeholder="Location" name="location" value={location} onChange={e => onChange(e)} />
-                <small className="form-text"
-                    >City & state suggested (eg. Boston, MA)</small
-                >
+                    <input 
+                        type="text" 
+                        placeholder="* Skills" 
+                        name="skills" 
+                        value={skills} 
+                        onChange={e => onChange(e)}
+                    />
+                    <small className="form-text">
+                        Please use comma separated values (eg.
+                        HTML,CSS,JavaScript,PHP)
+                    </small>
                 </div>
-                <div className="form-group">
-                <input type="text" placeholder="* Skills" name="skills" value={skills} onChange={e => onChange(e)}/>
-                <small className="form-text"
-                    >Please use comma separated values (eg.
-                    HTML,CSS,JavaScript,PHP)</small
-                >
-                </div>
+
                 {/*<div className="form-group">
                 <input
                     type="text"
@@ -107,16 +133,17 @@ const CreateProfile = ({ createProfile, history }) => {
                     username</small
                 >
                 </div>*/}
+
                 <div className="form-group">
-                <textarea placeholder="A short bio of yourself" name="bio" value={bio} onChange={e => onChange(e)}></textarea>
-                <small className="form-text">Tell us a little about yourself</small>
+                    <textarea placeholder="A short bio of yourself" name="bio" value={bio} onChange={e => onChange(e)}></textarea>
+                    <small className="form-text">Tell us a little about yourself</small>
                 </div>
 
                 <div className="my-2">
-                <button onClick={() => toggleSocialInputs(!displaySocialInputs)} type="button" className="btn btn-light">
-                    Add Social Network Links
-                </button>
-                <span>Optional</span>
+                    <button onClick={() => toggleSocialInputs(!displaySocialInputs)} type="button" className="btn btn-light">
+                        Add Social Network Links
+                    </button>
+                    <span>Optional</span>
                 </div>
 
                 {displaySocialInputs && <div>

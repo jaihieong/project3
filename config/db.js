e48dcc71bc764e664bf423d25fcdc8faeed67a7e
Pseudo Ninja <jaihieong88@gmail.com>
2019-05-29 19:28:20 -0400

    update before heroke deployment

--- config/db.js
+++ config/db.js
@@ -18,13 +18,4 @@ const connectDB = async () => {
   }
 };
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-
-mongoose.connect(
-  process.env.MONGODB_URI ||
-    "mongodb://heroku_f2dc1gr7:75aYUR99_R6g2rlp4q1p6uwuOVE69ZuA@ds155278.mlab.com:55278/heroku_f2dc1gr7"
-);
-
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
+const path = require("path");
 
 //Connect DB
 connectDB();
@@ -9,12 +10,22 @@ connectDB();
 app.use(express.json({ extended: false }));
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => res.send("API Running"));
+// app.get("/", (req, res) => res.send("API Running"));
 
 // Define Routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+// serve static assets in proudction
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
